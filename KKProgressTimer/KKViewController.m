1b03f68662b53274e8d9ed6142427287a84b85c0
gin0606 <k.kennichi.knlnshnm@gmail.com>
2013-09-04 21:32:27 +0900

    add views

--- KKProgressTimer/KKViewController.m
+++ KKProgressTimer/KKViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "KKViewController.h"
+#import "KKProgressTimer.h"
 
 @interface KKViewController ()
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UIView *view2;
+@property (weak, nonatomic) IBOutlet UIView *view3;
+@property (weak, nonatomic) IBOutlet UIView *view4;
 
 @end
 
@@ -18,6 +23,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    KKProgressTimer *timer1 = [[KKProgressTimer alloc] initWithFrame:self.view1.bounds];
+    KKProgressTimer *timer2 = [[KKProgressTimer alloc] initWithFrame:self.view2.bounds];
+    KKProgressTimer *timer3 = [[KKProgressTimer alloc] initWithFrame:self.view3.bounds];
+    KKProgressTimer *timer4 = [[KKProgressTimer alloc] initWithFrame:self.view4.bounds];
+    [self.view1 addSubview:timer1];
+    [self.view2 addSubview:timer2];
+    [self.view3 addSubview:timer3];
+    [self.view4 addSubview:timer4];
 }
 
 - (void)didReceiveMemoryWarning
